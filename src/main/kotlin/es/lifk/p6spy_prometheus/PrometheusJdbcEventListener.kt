87dfe25b64c866bd27f2975748baaf681e84504b
Luis Jurado <luisbcr@gmail.com>
2019-03-02 20:35:18 +0100

    change buckets

--- src/main/kotlin/es/lifk/p6spy_prometheus/PrometheusJdbcEventListener.kt
+++ src/main/kotlin/es/lifk/p6spy_prometheus/PrometheusJdbcEventListener.kt
@@ -11,7 +11,11 @@ import java.util.concurrent.TimeUnit
 class PrometheusJdbcEventListener: SimpleJdbcEventListener() {
     private val queries = Counter.build().name("queries_total").labelNames("status").help("Total queries.").register()
     private val inProgressQueries = Gauge.build().name("in_progress_queries").help("In progress queries.").register()
-    private val queryLatency = Histogram.build().name("query_latency_milis").help("query latency in milis").register()
+    private val queryLatency = Histogram.build()
+        .buckets(0.1, 1.0, 5.0, 10.0, 20.0, 40.0, 80.0, 160.0, 320.0, 640.0, 1280.0, 2560.0, 5120.0, 10240.0, 20580.0, 41060.0)
+        .name("query_latency_milis")
+        .help("query latency in milis")
+        .register()
 
     override fun onBeforeAnyExecute(statementInformation: StatementInformation) {
         inProgressQueries.inc()
